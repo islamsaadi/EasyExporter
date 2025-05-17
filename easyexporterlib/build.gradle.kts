e74b2ec5ba8a499825a4599d38545d71a9bd3c6d
Islam Saadi <islams@mail.afeka.ac.il>
2025-05-17 19:35:00 +0300

    First Release v1.0.0

--- easyexporterlib/build.gradle.kts
+++ easyexporterlib/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     alias(libs.plugins.android.library)
-    id("maven-publish")
+    alias(libs.plugins.maven.publish)
 }
 
 
@@ -25,8 +25,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
@@ -47,4 +47,42 @@ dependencies {
     implementation(libs.opencsv)
     // PDF: Android PdfDocument is built-in; for iText:
     implementation(libs.itextpdf)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                groupId = project.group.toString()
+                artifactId = "easyexporter"
+                version = project.version.toString()
+
+                artifact(tasks.getByName("bundleReleaseAar"))
+
+                pom {
+                    withXml {
+                        val depsNode = asNode().appendNode("dependencies")
+                        configurations["api"].dependencies.forEach { dep ->
+                            val dependencyNode = depsNode.appendNode("dependency")
+                            dependencyNode.appendNode("groupId", dep.group)
+                            dependencyNode.appendNode("artifactId", dep.name)
+                            dependencyNode.appendNode("version", dep.version)
+                            dependencyNode.appendNode("scope", "compile")
+                        }
+                        configurations["implementation"].dependencies.forEach { dep ->
+                            val dependencyNode = depsNode.appendNode("dependency")
+                            dependencyNode.appendNode("groupId", dep.group)
+                            dependencyNode.appendNode("artifactId", dep.name)
+                            dependencyNode.appendNode("version", dep.version)
+                            dependencyNode.appendNode("scope", "runtime")
+                        }
+                    }
+                }
+            }
+        }
+
+        repositories {
+            mavenLocal()
+        }
+    }
 }
